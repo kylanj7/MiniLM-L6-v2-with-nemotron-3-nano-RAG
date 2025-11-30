dd705c8e0a40389f885eda4b2cc00b842c0c31d7
Kylan Michael Johnson <103906447+kylanj7@users.noreply.github.com>
2025-11-30 12:48:08 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,8 +18,6 @@ fi
 echo "Activating virtual environment and installing requirements..."
 source "$VENV_DIR/bin/activate"
 
-pip install -r requirements.txt
-
 if [ -f "requirements.txt" ]; then
     pip install -r requirements.txt
     echo "Python requirements installed."
